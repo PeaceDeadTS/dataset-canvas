e65a98e3ff911e4a5f1dc70df5ccba7825be19e2
PeaceDeadC <peacedeadc660@gmail.com>
2025-09-10 17:23:23 +0300

    feat: new implement

--- src/pages/Dataset.tsx
+++ src/pages/Dataset.tsx
@@ -346,9 +346,9 @@ const DatasetPage = () => {
                     <TableHeader>
                       <TableRow>
                         <TableHead className="w-10">Row</TableHead>
-                        <TableHead className="min-w-[10rem] max-w-[32rem]">Image Key</TableHead>
-                        <TableHead className="min-w-[10rem] max-w-[44rem]">Filename</TableHead>
-                        <TableHead className="min-w-[10rem] max-w-[44rem]">Image</TableHead>
+                        <TableHead className="min-w-[30rem] max-w-[60rem]">Image Key</TableHead>
+                        <TableHead className="min-w-[20rem] max-w-[60rem]">Filename</TableHead>
+                        <TableHead className="min-w-[20rem] max-w-[60rem]">Image</TableHead>
                         <TableHead className="w-20">Dimensions</TableHead>
                         <TableHead className="min-w-[20rem]">Prompt</TableHead>
                       </TableRow>
